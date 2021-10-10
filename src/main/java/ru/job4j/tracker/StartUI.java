12b82c60754c994a62e2a19f4372ddcac81b61c4
Sonder82 <Himrs82@gmail.com>
2021-10-10 12:53:21 +0500

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745 #214257]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,53 +2,55 @@ package ru.job4j.tracker;
 
 import java.util.Scanner;
 
- public class StartUI {
-     public void init(Scanner scanner, Tracker tracker) {
-         boolean run = true;
-         while (run) {
-             showMenu();
-             System.out.print("Select: ");
-             int select = Integer.parseInt(scanner.nextLine());
-             if (select == 0) {
-                 System.out.println("=== Create a new Item ====");
-                 System.out.print("Enter name: ");
-                 String name = scanner.nextLine();
-                 Item item = new Item(name);
-                 tracker.add(item);
-                 System.out.println("Добавленная заявка: " + item);
-             } else if (select == 6) {
-                 run = false;
-             } else if (select == 1) {
-                 System.out.println("=== Show all items ====");
-                 Item[] items = tracker.findAll();
-                 if (items.length > 0) {
-                     for (Item item : items) {
-                         System.out.println(item);
-                     }
-                 } else {
-                     System.out.println("Хранилище еще не содержит заявок");
-                 }
-             }
-         }
-     }
-     private void showMenu() {
-         String[] menu = {
-                 "Add new Item", "Show all items", "Edit item",
-                 "Delete item", "Find item by id", "Find items by name",
-                 "Exit Program"
-         };
-         System.out.println("Menu:");
-         for (int index = 0; index < menu.length; index++) {
-             System.out.println(index + "." + menu[index]);
+public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Create a new Item ====");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Show all items ====");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
+                }
+            } else if (select == 6) {
+                run = false;
+            }
 
-         }
+        }
+    }
 
-     }
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int index = 0; index < menu.length; index++) {
+            System.out.println(index + "." + menu[index]);
 
-     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         Tracker tracker = new Tracker();
-         new StartUI().init(scanner,tracker);
+        }
 
-     }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
+
+    }
 }
