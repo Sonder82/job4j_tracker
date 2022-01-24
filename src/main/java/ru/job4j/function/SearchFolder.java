2562ffa1c5fdd31219524e5f37a4141c43a73e68
Sonder82 <Himrs82@gmail.com>
2022-01-24 12:39:53 +0500

    1.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,30 @@
+package ru.job4j.function;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * Класс опиывает совместную работу шаблона проектирования стратегия в сочетании с функциональными интерфейсами
+ * @author Alexey Novoselov
+ * @version 1.0
+ */
+
+public class SearchFolder {
+    /**
+     * Метод выполняет поиск элементов с помощью фильтра
+     * @param list список элементов коллекции
+     * @param pred функциональный интерфейс Predicate
+     * @return возвращает коллекцию элементов после фильтра
+     */
+
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> pred) {
+        List<Folder> rsl = new ArrayList<>();
+        for (Folder f : list) {
+           if (pred.test(f)) {
+               rsl.add(f);
+           }
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/function/StrategyUsage.java
@@ -0,0 +1,46 @@
+package ru.job4j.function;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Класс опиывает совместную работу шаблона проектирования стратегия в сочетании с функциональными интерфейсами
+ * @author Alexey Novoselov
+ * @version 1.0
+ */
+
+public class StrategyUsage {
+    /**
+     * Метод выполняет проверки с стороками
+     * @param pred функциональный интерфей Predicate
+     * @param s строка для которой выполняется проверка
+     * @return возвращает результат проверки строки
+     */
+    public boolean check (Predicate<String> pred, String s) {
+        return pred.test(s);
+    }
+
+    /**
+     * Метод выполняет преобразование строк
+     * @param fun функциональный интерфейс Function
+     * @param s строка с которой выполняется преобразование
+     * @return возвращает результат преобразования
+     */
+    public String transform(Function<String, String> fun, String s) {
+        return fun.apply(s);
+    }
+
+    public static void main(String[] args) {
+        StrategyUsage usage = new StrategyUsage();
+        System.out.println("Результат работы: " + usage.check(str -> str.isEmpty(), ""));
+        System.out.println("Результат работы: " + usage.check(str -> str.startsWith("Fun"), "Functional interface"));
+        System.out.println("Результат работы: " + usage.check(str -> str.contains("rn"), "Surname Name"));
+        System.out.println(
+                "Строка после преобразования: " + usage.transform(str -> str.toUpperCase(), "aBCdEfghKLmnpRstU"));
+        System.out.println(
+                "Строка после преобразования: " + usage.transform(
+                        str -> str.concat("работает корректно."), "Строка после преобразования: "));
+        System.out.println(
+        "Строка после преобразования: " + usage.transform(str -> str.trim(), "    aBC dEfghK Lmnp RstU        "));
+    }
+}
