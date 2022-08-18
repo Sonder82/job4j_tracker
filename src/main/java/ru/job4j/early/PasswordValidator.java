a42e57d1f77619e4557cd208844c4b03631bb865
Sonder82 <Himrs82@gmail.com>
2022-08-18 18:22:42 +0500

    4. Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,49 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("Password could not be null");
+        }
+        if (checkMatches(password)) {
+            throw new IllegalArgumentException("Password contains multiple words");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password length must be between 8 and 32 ");
+        }
+        checkCharacter(password);
+        return password;
+    }
+
+    private static void checkCharacter(String str) {
+        char ch;
+        for (int i = 0; i < str.length(); i++) {
+            ch = str.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                throw new IllegalArgumentException("Password contains Uppercase letter");
+            }
+            if (Character.isLowerCase(ch)) {
+                throw new IllegalArgumentException("Password contains Lowercase letter");
+            }
+            if (Character.isDigit(ch)) {
+                throw new IllegalArgumentException("Password contains Digital");
+            }
+            if (!Character.isLetterOrDigit(ch)) {
+                throw  new IllegalArgumentException("Password contains Special character");
+            }
+        }
+    }
+
+    private static boolean checkMatches (String str) {
+        boolean result = false;
+        String[] matches = new String[] {"qwerty", "12345", "password", "admin", "user"};
+        for (String s : matches) {
+            if (str.toUpperCase().contains(s.toUpperCase())) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -0,0 +1,63 @@
+package ru.job4j.early;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class PasswordValidatorTest {
+
+    @Test
+    void checkAtNull() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate(null))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("null");
+    }
+
+    @Test
+    void checkAtLength() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate("first"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("length");
+    }
+
+    @Test
+    void checkAtUpperCase() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate("VALIDATOR"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Uppercase");
+    }
+
+    @Test
+    void checkAtLowerCase() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate("vaLIDATOR"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Lowercase");
+    }
+
+    @Test
+    void checkAtDigit() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate("1VALIDATOR"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Digital");
+    }
+
+    @Test
+    void checkAtSpecial() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate("@VALIDATOR"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Special");
+    }
+    @Test
+    void checkAtMultiple() {
+        PasswordValidator pv = new PasswordValidator();
+        assertThatThrownBy(() -> pv.validate("password1"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("multiple");
+    }
+}
