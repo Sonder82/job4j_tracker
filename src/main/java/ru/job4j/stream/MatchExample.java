276bbca354b3dfba72e647d6fc5229d7af3d398c
Sonder82 <Himrs82@gmail.com>
2022-11-14 15:58:57 +0500

    16. Методы проверки элементов потока на соответствие условию [#504926 #360674]

---
+++ src/main/java/ru/job4j/stream/MatchExample.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MatchExample {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("Один", "Два", "Три", "Четыре", "Пять");
+        boolean rsl = strings.stream()
+                .allMatch(s -> s.endsWith("ь"));
+        System.out.println(rsl);
+    }
+}
